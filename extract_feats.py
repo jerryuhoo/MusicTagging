0404a6ca1cfb459792397269a4ce3f6cf0234950
jerryuhoo <jerryuhoo@gmail.com>
2023-02-28 02:37:06 -0500

    add multiprocess

--- extract_feats.py
+++ extract_feats.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import h5py
 import warnings
+import multiprocessing
 
 warnings.filterwarnings("ignore")
 
@@ -30,6 +31,32 @@ def extract_log_mel(y, sr, n_mels=128, fmin=20, fmax=8000, n_fft=2048, hop_lengt
     log_mel_spec = librosa.power_to_db(mel_spec)
     return log_mel_spec
 
+def preprocess_segment(args):
+    i, audio_data, row_idx, seg_count, sr, window_length, step, label_names, row, save_dir = args
+    pad_length = 30 * sr - len(audio_data)
+    x = np.pad(audio_data, (0, pad_length), mode="constant", constant_values=0)
+    segment_list = []
+    for i in range(0, len(x) - window_length + 1, step):
+        # get the current segment
+        end = i + window_length if i + window_length < len(x) else len(x)
+        segment = x[i:end]
+        segment_list.append(segment)
+    for idx, segment in enumerate(segment_list):
+        mfcc = extract_mfcc(y=segment, sr=sr, n_mfcc=25)
+        mfcc_mean = extract_mfcc_mean(mfcc)
+        log_mel = extract_log_mel(y=segment, sr=sr)
+        y = []
+        for col_idx, label_name in enumerate(label_names):
+            if label_name == "mp3_path" or label_name == "features_id":
+                continue
+            # Extract label
+            label = row[col_idx]
+            y.append(label)
+        np.save(os.path.join(save_dir, "label", f"label_{row_idx}_{seg_count + idx}.npy"), np.array(y))
+        np.save(os.path.join(save_dir, "mfcc", f"mfcc_{row_idx}_{seg_count + idx}.npy"), mfcc)
+        np.save(os.path.join(save_dir, "mfcc_mean", f"mfcc_mean_{row_idx}_{seg_count + idx}.npy"), mfcc_mean)
+        np.save(os.path.join(save_dir, "log_mel", f"log_mel_{row_idx}_{seg_count + idx}.npy"), log_mel)
+
 def preprocess_data(song_dir, csv_dir, save_dir):
     os.makedirs(os.path.join(save_dir, "mfcc"), exist_ok=True)
     os.makedirs(os.path.join(save_dir, "mfcc_mean"), exist_ok=True)
@@ -40,67 +67,29 @@ def preprocess_data(song_dir, csv_dir, save_dir):
     total_length = 30 * sr
     window_length = 10 * sr
     step = 5 * sr
-    seg_count = (30 - 5) // 5
 
     # load csv
     df = pd.read_csv(csv_dir, sep="\t")
     label_names = df.columns.values
+    pool = multiprocessing.Pool(processes=2)
+    results = []
     for row_idx, row in tqdm(df.iterrows(), total=df.shape[0]):
-        # Load the audio file
-        audio_path = os.path.join(song_dir, row["mp3_path"])[:-4] + ".wav"
+        audio_path = os.path.join(song_dir, row["mp3_path"])[:-4] + ".mp3"
         base_name = os.path.basename(audio_path)
         audio_name = base_name.split(".")[0]
         seg_count = 0
         try:
-            x, sr = librosa.load(audio_path, sr=sr)
-            # pad zero
-            pad_length = 30 * sr - len(x)
-            x = np.pad(x, (0, pad_length), mode="constant", constant_values=0)
-            for i in range(0, len(x) - window_length + 1, step):
-                # get the current segment
-                end = i + window_length if i + window_length < len(x) else len(x)
-                segment = x[i:end]
-
-                mfcc = extract_mfcc(y=segment, sr=sr, n_mfcc=25)
-                mfcc_mean = extract_mfcc_mean(mfcc)
-                log_mel = extract_log_mel(y=segment, sr=sr)
-                y = []
-                for col_idx, label_name in enumerate(label_names):
-                    if label_name == "mp3_path" or label_name == "features_id":
-                        continue
-                    # Extract label
-                    label = row[col_idx]
-                    y.append(label)
-                np.save(os.path.join(save_dir, "label", f"label_{row_idx}_{seg_count}.npy"), np.array(y))
-                np.save(os.path.join(save_dir, "mfcc", f"mfcc_{row_idx}_{seg_count}.npy"), mfcc)
-                np.save(os.path.join(save_dir, "mfcc_mean", f"mfcc_mean_{row_idx}_{seg_count}.npy"), mfcc_mean)
-                np.save(os.path.join(save_dir, "log_mel", f"log_mel_{row_idx}_{seg_count}.npy"), log_mel)
-                seg_count = seg_count + 1
+            audio_data, _ = librosa.load(audio_path, sr=sr)
+            args_list = [(i, audio_data, row_idx, seg_count, sr, window_length, step, label_names, row, save_dir) for i in range(0, len(audio_data) - window_length + 1, step)]
+            results.append(pool.map_async(preprocess_segment, args_list))
+            seg_count += len(args_list)
         except Exception:
             print("error: {}".format(audio_path))
-
-    # if feats_type == "log_mel":
-    #     np.save(os.path.join(save_dir, "log_mel", "log_mel.npy"), X)
-
-    # X = np.array(X)
-    # Normalize the features
-    # X_mean = np.mean(X, axis=0)
-    # X_var = np.std(X, axis=0)
-    # X = X - X_mean / X_var
-    # np.save("mean.npy", X_mean)
-    # np.save("var.npy", X_var)
-    # np.save(os.path.join(save_dir, "audio_features.npy"), X)
-
-    """
-    # load mean and variance from .npy files
-    mean = np.load("mean.npy")
-    var = np.load("var.npy")
-
-    # use mean and variance to normalize new data
-    X_new = ...
-    X_new = (X_new - mean) / var
-    """
-    return None
+    pool.close()
+    pool.join()
+    for res in results:
+        res.wait()
+    print("done")
 
 def create_hdf5_dataset(data_dir, save_dir, dataset_name, batch_size):
     # Get a list of filenames sorted alphabetically
