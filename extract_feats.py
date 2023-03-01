9f829d477eb30ed8e9f84300fb8187e61db9ebd9
jerryuhoo <jerryuhoo@gmail.com>
2023-02-28 22:41:19 -0500

    fix

--- extract_feats.py
+++ extract_feats.py
@@ -8,7 +8,6 @@ import warnings
 import multiprocessing
 from memory_profiler import profile, memory_usage
 
-
 warnings.filterwarnings("ignore")
 
 # Define the number of MFCCs to extract
@@ -34,15 +33,13 @@ def extract_log_mel(y, sr, n_mels=128, fmin=20, fmax=8000, n_fft=2048, hop_lengt
     return log_mel_spec
 
 def preprocess_segment(args):
-    # i, audio_data, row_idx, seg_count, sr, window_length, step, row, save_dir = args
-    song_dir, row_idx, row, seg_idx, sr, window_length, step, save_dir = args
-    audio_path = os.path.join(song_dir, row["mp3_path"])[:-4] + ".mp3"
+    song_dir, row_idx, row, sr, window_length, step, save_dir = args
+    # print("row_idx",row_idx)
+    audio_path = os.path.splitext(os.path.join(song_dir, row["mp3_path"]))[0] + ".mp3"
     base_name = os.path.basename(audio_path)
-    audio_name = base_name.split(".")[0]
-    seg_count = 0
+    # audio_name = base_name.split(".")[0]
     try:
         audio_data, _ = librosa.load(audio_path, sr=sr)
-    
         pad_length = 30 * sr - len(audio_data)
         x = np.pad(audio_data, (0, pad_length), mode="constant", constant_values=0)
         segment_list = []
@@ -51,15 +48,15 @@ def preprocess_segment(args):
             end = i + window_length if i + window_length < len(x) else len(x)
             segment = x[i:end]
             segment_list.append(segment)
-        for idx, segment in enumerate(segment_list):
+        for seg_idx, segment in enumerate(segment_list):
             mfcc = extract_mfcc(y=segment, sr=sr, n_mfcc=25)
             mfcc_mean = extract_mfcc_mean(mfcc)
             log_mel = extract_log_mel(y=segment, sr=sr)
             y = np.array(list(row["features_id"].split("_")[0]), dtype=int)
-            np.save(os.path.join(save_dir, "label", f"label_{row_idx}_{seg_count + idx}.npy"), np.array(y))
-            np.save(os.path.join(save_dir, "mfcc", f"mfcc_{row_idx}_{seg_count + idx}.npy"), mfcc)
-            np.save(os.path.join(save_dir, "mfcc_mean", f"mfcc_mean_{row_idx}_{seg_count + idx}.npy"), mfcc_mean)
-            np.save(os.path.join(save_dir, "log_mel", f"log_mel_{row_idx}_{seg_count + idx}.npy"), log_mel)
+            np.save(os.path.join(save_dir, "label", f"label_{row_idx}_{seg_idx}.npy"), y)
+            np.save(os.path.join(save_dir, "mfcc", f"mfcc_{row_idx}_{seg_idx}.npy"), mfcc)
+            np.save(os.path.join(save_dir, "mfcc_mean", f"mfcc_mean_{row_idx}_{seg_idx}.npy"), mfcc_mean)
+            np.save(os.path.join(save_dir, "log_mel", f"log_mel_{row_idx}_{seg_idx}.npy"), log_mel)
     except Exception:
         print("error: {}".format(audio_path))
 
@@ -79,40 +76,24 @@ def preprocess_data(song_dir, csv_dir, save_dir, n_workers=2):
     df = pd.read_csv(csv_dir, sep="\t")
     df = df.iloc[:, -2:]
     pool = multiprocessing.Pool(processes=n_workers)
-    results = []
-    
-    # for row_idx, row in tqdm(df.iterrows(), total=df.shape[0]):
-    #     # print("Memory usage:", memory_usage()[0])
-    #     try:
-    #         audio_path = os.path.join(song_dir, row["mp3_path"])[:-4] + ".mp3"
-    #         base_name = os.path.basename(audio_path)
-    #         audio_name = base_name.split(".")[0]
-    #         seg_count = 0
-    #         audio_data, _ = librosa.load(audio_path, sr=sr)
-    #         args_list = [(i, audio_data, row_idx, seg_count, sr, window_length, step, row, save_dir) for i in range(0, len(audio_data) - window_length + 1, step)]
-    #         # results.append(pool.map_async(preprocess_segment, args_list))
-    #         pool.map_async(preprocess_segment, args_list)
-    #         seg_count += len(args_list)
-    #     except Exception:
-    #         print("error: {}".format(audio_path))
-
 
     args_list = []
     for row_idx, row in tqdm(df.iterrows(), total=df.shape[0]):
-        for seg_idx in range(seg_count):
-            args_list.append((song_dir, row_idx, row, seg_idx, sr, window_length, step, save_dir))
-            pool.map_async(preprocess_segment, args_list)
+        try:  
+            args_list.append((song_dir, row_idx, row, sr, window_length, step, save_dir))
+        except Exception:
+            audio_path = os.path.join(song_dir, row["mp3_path"])[:-4] + ".mp3"
+            print("error: {}".format(audio_path))
 
-    # with multiprocessing.Pool(processes=n_workers) as pool:
-    #     pool.map_async(preprocess_segment, args_list).get()
-    #     pool.close()
-    #     pool.join()
-        # mem_usage_end = memory_usage()[0]
-        # print("Memory usage:", mem_usage_end - mem_usage_start, "MB")
+    with tqdm(total=len(args_list)) as pbar:
+        for _ in pool.imap_unordered(preprocess_segment, args_list):
+            pbar.update() 
+
+    # results = pool.map_async(preprocess_segment, args_list)
+
     pool.close()
     pool.join()
-    for res in results:
-        res.wait()
+
     print("done")
 
 def create_hdf5_dataset(data_dir, save_dir, dataset_name, batch_size):
