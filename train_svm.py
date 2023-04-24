589e98060dabfdcbd443ddd11efd49409b1f60d1
jerryuhoo <jerryuhoo@gmail.com>
2023-04-24 13:53:22 -0400

    fix SVM train

--- train_svm.py
+++ train_svm.py
@@ -8,7 +8,7 @@ import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
-
+from sklearn.preprocessing import StandardScaler
 from imblearn.under_sampling import RandomUnderSampler
 import h5py
 
@@ -17,18 +17,31 @@ n_components = 25
 plot_graph = False
 
 with h5py.File("preprocessed/10/training/mfcc.h5", "r") as f:
-    X = f["mfcc"][:]
+    X_train = f["mfcc"][:]
 
 with h5py.File("preprocessed/10/training/label.h5", "r") as f:
-    y = f["label"][:]
+    y_train = f["label"][:]
 
-X = np.array(X)
-X = np.mean(X, axis=-1)
-y_all = np.array(y)
-print(X.shape)
-print(y_all.shape)
+with h5py.File("preprocessed/10/testing/mfcc.h5", "r") as f:
+    X_test = f["mfcc"][:]
 
-assert X.shape[0] == y_all.shape[0]
+with h5py.File("preprocessed/10/testing/label.h5", "r") as f:
+    y_test = f["label"][:]
+
+X_train = np.array(X_train)
+X_train = np.mean(X_train, axis=-1)
+y_train = np.array(y_train)
+
+X_test = np.array(X_test)
+X_test = np.mean(X_test, axis=-1)
+y_test = np.array(y_test)
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+assert X_train.shape[0] == y_train.shape[0]
+assert X_test.shape[0] == y_test.shape[0]
 
 # load csv data to get column names
 csv_dir = "../data/magnatagatune/annotations_final_new.csv"
@@ -38,59 +51,53 @@ n_cols = df.shape[1] - 2
 
 # loop through all labels
 results = []
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# pca = PCA(n_components=n_components)
+# X = pca.fit_transform(X)
+# print("after PCA", X.shape)
+
 for label_index in range(0, n_cols):
-    y = y_all[:, label_index]
+    print("label_index", label_index)
+    y_train_one_label = y_train[:, label_index]
+    y_test_one_label = y_test[:, label_index]
 
     current_label = label_names[label_index]
     print("now training:", current_label)
-    # print("before PCA", X.shape)
-
-    pca = PCA(n_components=n_components)
-    X = pca.fit_transform(X)
-    # print("after PCA", X.shape)
-
-    from sklearn.preprocessing import StandardScaler
-
-    scaler = StandardScaler()
-    X_normalized = scaler.fit_transform(X)
-    # print(scaler.mean_)  # Prints the mean of each feature
-    # print(scaler.var_)  # Prints the variance of each feature
-    # np.save("preprocessed/mean.npy", X_mean)
-    # np.save("preprocessed/var.npy", X_var)
-    # print(X_normalized)
 
     # Initialize the sampler
     print("under sample:")
     sampler = RandomUnderSampler(sampling_strategy="majority")
 
     # Fit and apply the sampler to your data
-    X_resampled, y_resampled = sampler.fit_resample(X_normalized, y)
+    X_resampled, y_resampled = sampler.fit_resample(X_train, y_train_one_label)
 
-    # Split the data into training and test sets
-    print("split:")
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_resampled, y_resampled, test_size=0.2, random_state=42
-    )
+    # X_test_resampled, y_test_one_label_resampled = sampler.fit_resample(
+    #     X_test, y_test_one_label
+    # )
 
-    print("Number of positive labels in training data:", sum(y_train == 1))
-    print("Number of negative labels in training data:", sum(y_train == 0))
-    print("Number of positive labels in testing data:", sum(y_test == 1))
-    print("Number of negative labels in testing data:", sum(y_test == 0))
+    print("Number of positive labels in training data:", sum(y_train_one_label == 1))
+    print("Number of negative labels in training data:", sum(y_train_one_label == 0))
+    print("Number of positive labels in resampled data:", sum(y_resampled == 1))
+    print("Number of negative labels in resampled data:", sum(y_resampled == 0))
 
     # Train the SVM model
     clf = svm.SVC(kernel="linear", C=1, random_state=42, verbose=True)
-    clf.fit(X_train, y_train)
+    clf.fit(X_resampled, y_resampled)
 
     # Evaluate the model on the test set
-    # X_test_normalized = scaler.transform(X_test)
-    accuracy = clf.score(X_test, y_test)
+    accuracy = clf.score(X_test, y_test_one_label)
+    # accuracy = clf.score(X_test_resampled, y_test_one_label_resampled)
     print("Accuracy:", accuracy)
 
     # Predict the labels for the test data using the trained model
     y_pred = clf.predict(X_test)
-    # print("y_pred", y_pred)
-    # Calculate the F1-score for the predictions
-    f1 = f1_score(y_test, y_pred)
+    f1 = f1_score(y_test_one_label, y_pred)
+    # y_pred = clf.predict(X_test_resampled)
+    # f1 = f1_score(y_test_one_label_resampled, y_pred)
 
     # Print the F1-score
     print("F1-score:", f1)
@@ -148,8 +155,23 @@ for label_index in range(0, n_cols):
         plt.clf()
         print(current_label + " finished!")
 
+# extract the accuracy and f1 scores into separate lists
+accuracies = [x[1] for x in results]
+f1_scores = [x[2] for x in results]
+
+# compute the mean accuracy and f1 score using numpy.mean()
+mean_accuracy = np.mean(accuracies)
+mean_f1 = np.mean(f1_scores)
+
+average = ["average", mean_accuracy, mean_f1]
 
 with open("list_file_" + str(n_components) + ".txt", "w") as file:
     for row in results:
         print(row)
         file.write(str(row) + "\n")
+    file.write(str(average) + "\n")
+
+# print the results
+print("Mean accuracy:", mean_accuracy)
+print("Mean F1 score:", mean_f1)
+print("Done!")
